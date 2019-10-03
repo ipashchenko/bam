78d133b3242737315cef6008c779103d8e754ee1
Ilya Pashchenko <in4pashchenko@gmail.com>
2019-10-03 15:23:33 +0300

    No way to realize robust symmetric (re, im) noise.

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,8 @@
 #include "Data.h"
 #include "DNestModel.h"
 #include "DNest4.h"
-#include "stats.hpp"
-#include "gcem.hpp"
-
 
 using namespace DNest4;
-
-
-//void test_student() {
-//    // parameters
-//    double dof = 14.0;
-//
-//// standard input
-//    double dens_val = stats::dt(0.5,dof);
-//    double log_dens_val = stats::dt(0.5,dof,true);
-//    std::cout << log_dens_val << std::endl;
-//}
 
 
 // On server
@@ -33,7 +19,3 @@ int main(int argc, char** argv)
 
     return 0;
 }
-
-//int main() {
-//    test_student();
-//}
