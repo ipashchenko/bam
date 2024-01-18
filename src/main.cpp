fe7fa280d6235d56f11aca5eaecc750428c3cdc4
ilya Pashchenko <in4pashchenko@gmail.com>
2024-01-18 16:37:42 +0300

    main using command line options.

--- src/main.cpp
+++ src/main.cpp
@@ -13,23 +13,24 @@ const bool use_offsets = false;
 
 int main(int argc, char** argv)
 {
-	Data::get_instance().load("/home/ilya/Downloads/mojave/0136+176/2011_07_24/0136+176.u.2011_07_24_60sec_antennas.csv");
-	// set the sampler and run it!
-	Sampler<DNestModel> sampler = setup<DNestModel>(argc, argv);
-	sampler.run();
-	
-	// Use custom OPTIONS and data file
-//	// Run DNest4
-//	CommandLineOptions options(argc, argv);
-//
-//	// Load sampler options from file
-//	Options sampler_options(options.get_options_file().c_str());
-//	sampler_options.print(std::cout);
-//
-//	Data::get_instance().load(options.get_data_file());
-//	Sampler<DNestModel> sampler = setup<DNestModel>(options);
+//	// OPTIONS from run directory, data file hard coded in main.
+//	Data::get_instance().load("/home/ilya/Downloads/mojave/0136+176/2011_07_24/0136+176.u.2011_07_24_60sec_antennas.csv");
+//	// set the sampler and run it!
+//	Sampler<DNestModel> sampler = setup<DNestModel>(argc, argv);
 //	sampler.run();
 	
+    // Use custom OPTIONS and data file
+	// Run DNest4
+	CommandLineOptions options(argc, argv);
+
+	// Load sampler options from file
+	Options sampler_options(options.get_options_file().c_str());
+	sampler_options.print(std::cout);
+	
+	const std::string& data_file = options.get_data_file();
+	Data::get_instance().load(data_file.c_str());
+	Sampler<DNestModel> sampler = setup<DNestModel>(options);
+	sampler.run();
 	
 	return 0;
 }
