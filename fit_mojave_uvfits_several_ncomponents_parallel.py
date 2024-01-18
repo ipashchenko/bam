1d36375bcc72767fc7d790cda829541ffa4c3f4d
ilya Pashchenko <in4pashchenko@gmail.com>
2024-01-18 21:34:08 +0300

    Ready for parallel run 6 recent epochs of 1502+106.

---
+++ fit_mojave_uvfits_several_ncomponents_parallel.py
@@ -0,0 +1,81 @@
+import sys
+import os
+import glob
+from data_utils import get_data_file_from_ehtim
+
+
+dry_run = False
+source = "1502+106"
+# cg, eg, sp. Not used now.
+component_type = "cg"
+# Directory with UVFITS files for this source in MOJAVE format
+uvfits_dir = "/home/ilya/Downloads/mojave/1502+106/uvfits_dir"
+# Or iterable of UVFITS files
+uvfits_files = None
+# Directory to save the results
+base_save_dir = "/home/ilya/data/rjbam"
+n_jobs = 6
+
+# FIXME: Choose executable depending on ``component_type``
+executable = "/home/ilya/github/bam/Release/bam"
+
+maxnsaves = 50000
+# Average time in difmap. It uses weighted vector averaging and also re-calculate weightsю This is helpful when they are
+# not reliable.
+difmap_avg_time_sec = 60
+template_options = "/home/ilya/github/bam/OPTIONS_gen"
+
+if uvfits_files is None:
+    uvfits_files = glob.glob(os.path.join(uvfits_dir, f"{source}*.uvf"))
+
+epochs = list()
+for uvfits_file in uvfits_files:
+    _, fn = os.path.split(uvfits_file)
+    epoch = fn.split(".")[2]
+    epochs.append(epoch)
+
+results_dir = os.path.join(base_save_dir, source)
+if not os.path.exists(results_dir):
+    os.mkdir(results_dir)
+results_dirs = list()
+for epoch in epochs:
+    results_dir_epoch = os.path.join(results_dir, epoch)
+    if not os.path.exists(results_dir_epoch):
+        os.mkdir(results_dir_epoch)
+    results_dirs.append(results_dir_epoch)
+
+out_fnames = list()
+basenames = list()
+for uvfits_file, epoch in zip(uvfits_files, epochs):
+    basename = f"{source}.u.{epoch}"
+    basenames.append(basename)
+    out_fname = os.path.join(base_save_dir, source, epoch, f"{basename}.csv")
+    out_fnames.append(out_fname)
+    df = get_data_file_from_ehtim(uvfits_file, out_fname, avg_time_sec=difmap_avg_time_sec, average_using="difmap")
+
+args = " ".join(["{:s} {:s} {:s}".format(i, j, k) for (i, j, k) in zip(basenames, out_fnames, results_dirs)])
+
+basenames = " ".join(basenames)
+out_fnames = " ".join(out_fnames)
+results_dirs = " ".join(results_dirs)
+
+print("basenames\n", basenames)
+print("outfnames\n", out_fnames)
+print("results_dirs\n", results_dirs)
+print("args\n", args)
+
+if dry_run:
+    os.system('parallel --files --results {}/res_{{1}} --joblog {}/joblog --jobs {} --dryrun --link '
+              '"python fit_mojave_uvfits_single_ncomponents.py --executable {} --template_options {} --basename {{1}} '
+              '--data_file {{2}} --results_dir {{3}}" ::: {} ::: {} ::: {}'.format(results_dir, results_dir, n_jobs,
+                                                                                   executable, template_options,
+                                                                                   basenames, out_fnames, results_dirs))
+else:
+    os.system('parallel --files --results {}/res_{{1}} --joblog {}/joblog --jobs {} --link '
+              '"python fit_mojave_uvfits_single_ncomponents.py --executable {} --template_options {} --basename {{1}} '
+              '--data_file {{2}} --results_dir {{3}}" ::: {} ::: {} ::: {}'.format(results_dir, results_dir, n_jobs,
+                                                                                   executable, template_options,
+                                                                                   basenames, out_fnames, results_dirs))
+
+# This works in bash
+# $ parallel -k  --link --dryrun "python fit_mojave_uvfits_single_ncomponents.py --uvfits \"uvfits\" --results_dir \"results_dir\" --ncomps {1} --maxnsaves {2}" ::: 1 2 ::: 1000 2000
---
+++ fit_mojave_uvfits_single_ncomponents.py
@@ -0,0 +1,83 @@
+import os
+import sys
+from string import Template
+import numpy as np
+import argparse
+sys.path.insert(0, '/home/ilya/github/dnest4post')
+from postprocess import postprocess
+
+
+def run(basename, maxnsaves, data_file, results_dir, template_options, excecutable):
+
+    run_name = basename
+    result_options = os.path.join(results_dir, "OPTIONS_{}".format(run_name))
+    options = {'nparticles': 1,
+               'newlevel': 200000,
+               'saveinterval': 20000,
+               'threadsteps': 100,
+               'maxnlevels': 0,
+               'lambda': 30,
+               'beta': 100,
+               'maxnsaves': maxnsaves,
+               'samplefile': '{}/sample_{}.txt'.format(results_dir, run_name),
+               'sampleinfo': '{}/sample_info_{}.txt'.format(results_dir, run_name),
+               'levelsfile': '{}/levels_{}.txt'.format(results_dir, run_name)}
+
+    filein = open(template_options)
+    src = Template(filein.read())
+    result = src.substitute(options)
+    with open(result_options, "w") as fo:
+        print(result, file=fo)
+
+    executable_dir = os.path.split(executable)[0]
+    os.chdir(executable_dir)
+    os.system("{} -t 5 -o {} -d {}".format(excecutable,
+                                           result_options,
+                                           data_file))
+    print("POSTPROCESSING DNEST OUTPUT...")
+    post_samples_file = os.path.join(results_dir, "posterior_sample_{}.txt".format(run_name))
+
+    logZ, _, _ = postprocess(plot=True, pics_prefix=f"{results_dir}/{basename}", show_pics=False,
+                             sample_file='{}/sample_{}.txt'.format(results_dir, run_name),
+                             level_file='{}/levels_{}.txt'.format(results_dir, run_name),
+                             sample_info_file='{}/sample_info_{}.txt'.format(results_dir, run_name),
+                             post_sample_file=post_samples_file)
+    os.system("touch {}/{}_logZ_{:.3f}.info".format(results_dir, run_name, logZ))
+    os.system("rm {}/sample_{}.txt".format(results_dir, run_name))
+    os.system("rm {}/levels_{}.txt".format(results_dir, run_name))
+    os.system("rm {}/sample_info_{}.txt".format(results_dir, run_name))
+
+    post_samples = np.loadtxt(post_samples_file)
+    n_post_samples = len(post_samples)
+    if n_post_samples < 1000:
+        os.system("touch {}/WARNING_SMALL_SAMPLE_SIZE_FOR_NCOMP{}_{}".format(results_dir, n_components, run_name))
+
+
+if __name__ == "__main__":
+
+    CLI = argparse.ArgumentParser()
+    CLI.add_argument("--basename",
+                     type=str)
+    CLI.add_argument("--data_file",
+                     type=str)
+    CLI.add_argument("--maxnsaves",
+                     type=int)
+    CLI.add_argument("--results_dir",
+                     type=str)
+    CLI.add_argument("--executable",
+                     type=str)
+    CLI.add_argument("--template_options",
+                     type=str)
+    args = CLI.parse_args()
+
+    basename = args.basename
+    data_file = args.data_file
+    maxnsaves = args.maxnsaves
+    results_dir = args.results_dir
+    executable = args.executable
+    template_options = args.template_options
+
+    print("Basename = ", basename)
+    print("Data file = ", data_file)
+    print("Results dir = ", results_dir)
+    run(basename, maxnsaves, data_file, results_dir, template_options, executable)
