42ad12efb088deee60a47fcae6ac6523e643f5ce
Ilya Pashchenko <in4pashchenko@gmail.com>
2024-05-16 00:03:37 +0300

    Choosing executable dpending on number of components

--- fit_norj_with_parallels.py
+++ fit_norj_with_parallels.py
@@ -15,7 +15,7 @@ def choose_maxnsaves(n_comps, n_vis):
         maxnsaves = 7000
     else:
         maxnsaves = 10000
-    return int(n_comps*maxnsaves/2)
+    return int(n_comps*maxnsaves/2/100)
 
 
 dry_run = False
@@ -29,13 +29,8 @@ base_save_dir = "/mnt/storage/ilya/VLBI_Gaia"
 n_jobs = 2
 
 # FIXME: Choose executable depending on ``component_type``
-executable_dict = {1: "/home/ilya/github/bam/Release/bam_1",
-                   2: "/home/ilya/github/bam/Release/bam_2",
-                   3: "/home/ilya/github/bam/Release/bam_3",
-                   4: "/home/ilya/github/bam/Release/bam_4",
-                   5: "/home/ilya/github/bam/Release/bam_5",
-                   6: "/home/ilya/github/bam/Release/bam_6",
-                   7: "/home/ilya/github/bam/Release/bam_7"}
+executable_dict = {2: "/home/ilya/github/bam/Release/bam_2",
+                   3: "/home/ilya/github/bam/Release/bam_3"}
 executable = "/home/ilya/github/bam/Release/bam"
 
 # Average time in difmap. It uses weighted vector averaging and
@@ -52,6 +47,7 @@ results_dirs = list()
 basenames = list()
 data_files = list()
 maxnsaves_list = list()
+executables = list()
 for row in df.itertuples():
     uvfits = row.uvfits
     ccfits = row.ccfits
@@ -75,13 +71,18 @@ for row in df.itertuples():
     maxnsaves = choose_maxnsaves(ncomps, n_vis)
     print("Chosen maxnsaves = ", maxnsaves)
     maxnsaves_list.append(str(maxnsaves))
-
+    n_components.append(ncomps)
+    try:
+        executables.append(executable_dict[ncomps])
+    except KeyError:
+        raise Exception(f"No executable for #components = {ncomps}!")
     data_files.append(out_fname)
 
 
 args = " ".join(["{:s} {:s} {:s}".format(i, j, k) for (i, j, k) in zip(basenames, data_files, results_dirs)])
 
 maxnsaves = " ".join(maxnsaves_list)
+executables = " ".join(executables)
 basenames = " ".join(basenames)
 data_files = " ".join(data_files)
 results_dirs = " ".join(results_dirs)
@@ -90,20 +91,21 @@ print("basenames\n", basenames)
 print("data_files\n", data_files)
 print("results_dirs\n", results_dirs)
 print("maxnsaves\n", maxnsaves)
+print("executables\n", executables)
 print("args\n", args)
 
 if dry_run:
     os.system('parallel --files --results {}/res_{{1}} --joblog {}/joblog --jobs {} --dryrun --link '
-              '"python fit_mojave_uvfits_single_ncomponents.py --executable {} --maxnsaves {} --template_options {} --basename {{1}} '
-              '--data_file {{2}} --results_dir {{3}}" ::: {} ::: {} ::: {}'.format(base_save_dir, base_save_dir, n_jobs,
-                                                                                   executable, maxnsaves, template_options,
-                                                                                   basenames, data_files, results_dirs))
+              '"python fit_mojave_uvfits_single_ncomponents.py --template_options {} --basename {{1}} '
+        '--data_file {{2}} --results_dir {{3}} --maxnsaves {{4}} --executable {{5}}" ::: {} ::: {} ::: {} ::: {} ::: {}'.format(base_save_dir, base_save_dir, n_jobs,
+                                                                                   template_options,
+                                                                                   basenames, data_files, results_dirs, maxnsaves, executables))
 else:
     os.system('parallel --files --results {}/res_{{1}} --joblog {}/joblog --jobs {} --link '
-              '"python fit_mojave_uvfits_single_ncomponents.py --executable {} --template_options {} --basename {{1}} '
-        '--data_file {{2}} --results_dir {{3}} --maxnsaves {{4}}" ::: {} ::: {} ::: {} ::: {}'.format(base_save_dir, base_save_dir, n_jobs,
-                                                                                   executable, template_options,
-                                                                                   basenames, data_files, results_dirs, maxnsaves))
+              '"python fit_mojave_uvfits_single_ncomponents.py --template_options {} --basename {{1}} '
+        '--data_file {{2}} --results_dir {{3}} --maxnsaves {{4}} --executable {{5}}" ::: {} ::: {} ::: {} ::: {} ::: {}'.format(base_save_dir, base_save_dir, n_jobs,
+                                                                                   template_options,
+                                                                                   basenames, data_files, results_dirs, maxnsaves, executables))
 
 # This works in bash
 # $ parallel -k  --link --dryrun "python fit_mojave_uvfits_single_ncomponents.py --uvfits \"uvfits\" --results_dir \"results_dir\" --ncomps {1} --maxnsaves {2}" ::: 1 2 ::: 1000 2000
