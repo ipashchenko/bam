644032df8584b3cb13280f9e9d56658c7e5fd1cb
Ilya Pashchenko <in4pashchenko@gmail.com>
2024-05-16 04:09:05 +0300

    Working, but I have to split df

--- fit_norj_with_parallels.py
+++ fit_norj_with_parallels.py
@@ -9,6 +9,7 @@ from tempfile import TemporaryDirectory
 
 def reformat_table(original_csv):
     df = pd.read_csv(original_csv)
+    csv_dir, _ = os.path.split(original_csv)
 
     uvfits_files = list()
     ccfits_files = list()
@@ -40,7 +41,7 @@ def reformat_table(original_csv):
 
     dic = {"uvfits": uvfits_files, "ccfits": ccfits_files, "ncomps": ncomps}
     df = pd.DataFrame.from_dict(dic)
-    df.to_csv("full_table_ncomp_2.csv", header=True, index=False)
+    df.to_csv(os.path.join(csv_dir, "full_table_ncomp_2.csv"), header=True, index=False)
     return df
 
 
@@ -53,97 +54,119 @@ def choose_maxnsaves(n_comps, n_vis):
         maxnsaves = 7000
     else:
         maxnsaves = 10000
-    return int(n_comps*maxnsaves/2/100)
+    return int(n_comps*maxnsaves/2)
 
 
-dry_run = False
-# table_file = "/home/ilya/data/VLBI_Gaia/obs_pairs_good_8.csv"
-table_file = "/home/ilya/data/VLBI_Gaia/test.csv"
+def run_parallels_on_df(df, base_save_dir, executable_dict, template_options, n_jobs, difmap_avg_time_sec, dry_run=False):
+
+
+
+    uvfits_files = list()
+    ccfits_file = list()
+    n_components = list()
+    results_dirs = list()
+    basenames = list()
+    data_files = list()
+    maxnsaves_list = list()
+    executables = list()
+    failed_uvfits = list()
+    for row in df.itertuples():
+        uvfits = row.uvfits
+        ccfits = row.ccfits
+        ncomps = row.ncomps
+
+
+        uvfits_fn = os.path.split(uvfits)[-1]
+        source, band, year, month, day, author, product = uvfits_fn.split("_")
+        product = product.split(".")[0]
+        assert product == "vis"
+        save_basename = f"{source}_{band}_{year}_{month}_{day}_{author}_ncomp_{ncomps}"
+        results_dir = os.path.join(base_save_dir, f"{source}_{band}_{year}_{month}_{day}_{author}")
+        pathlib.Path(results_dir).mkdir(parents=True, exist_ok=True)
+        out_fname = os.path.join(results_dir, f"{source}_{band}_{year}_{month}_{day}_{author}_avg_{difmap_avg_time_sec}sec.csv")
+        with TemporaryDirectory() as working_dir:
+            try:
+                df = get_data_file_from_ehtim(uvfits, out_fname, avg_time_sec=difmap_avg_time_sec, working_dir=working_dir)
+            except:
+                signal_file = os.path.join(base_save_dir, f"FAILED_CREATING_DATA_FILE_{source}_{band}_{year}_{month}_{day}_{author}.info")
+                os.system(f"touch {signal_file}")
+                failed_uvfits.append(uvfits)
+                continue
+
+        n_vis = len(df)
+        print("Number of vis = ", n_vis)
+        print("Number of comps = ", ncomps)
+        maxnsaves = choose_maxnsaves(ncomps, n_vis)
+        print("Chosen maxnsaves = ", maxnsaves)
+        # Append only if its OK with data file!
+        basenames.append(save_basename)
+        results_dirs.append(results_dir)
+        maxnsaves_list.append(str(maxnsaves))
+        n_components.append(ncomps)
+        data_files.append(out_fname)
+
+        try:
+            executables.append(executable_dict[ncomps])
+        except KeyError:
+            raise Exception(f"No executable for #components = {ncomps}!")
+
+
+    with open(os.path.join(base_save_dir, "failed_UVFITS.txt"), "w") as fo:
+        for fn in failed_uvfits:
+            fo.write(fn + "\n")
+
+    args = " ".join(["{:s} {:s} {:s}".format(i, j, k) for (i, j, k) in zip(basenames, data_files, results_dirs)])
+
+    maxnsaves = " ".join(maxnsaves_list)
+    executables = " ".join(executables)
+    basenames = " ".join(basenames)
+    data_files = " ".join(data_files)
+    results_dirs = " ".join(results_dirs)
+
+    print("basenames\n", basenames)
+    print("data_files\n", data_files)
+    print("results_dirs\n", results_dirs)
+    print("maxnsaves\n", maxnsaves)
+    print("executables\n", executables)
+
+    # 536 is OK!
+    # 609 failed!
+    print(f"Length of the argument is {len(maxnsaves_list)}")
+
+    if dry_run:
+        os.system('parallel --files --results {}/res_{{1}} --joblog {}/joblog --jobs {} --dryrun --link '
+                  '"python fit_mojave_uvfits_single_ncomponents.py --template_options {} --basename {{1}} '
+            '--data_file {{2}} --results_dir {{3}} --maxnsaves {{4}} --executable {{5}}" ::: {} ::: {} ::: {} ::: {} ::: {}'.format(base_save_dir, base_save_dir, n_jobs,
+                                                                                       template_options,
+                                                                                       basenames, data_files, results_dirs, maxnsaves, executables))
+    else:
+        os.system('parallel --files --results {}/res_{{1}} --joblog {}/joblog --jobs {} --link '
+                  '"python fit_mojave_uvfits_single_ncomponents.py --template_options {} --basename {{1}} '
+            '--data_file {{2}} --results_dir {{3}} --maxnsaves {{4}} --executable {{5}}" ::: {} ::: {} ::: {} ::: {} ::: {}'.format(base_save_dir, base_save_dir, n_jobs,
+                                                                                       template_options,
+                                                                                       basenames, data_files, results_dirs, maxnsaves, executables))
+    df_log = pd.read_csv(f"{base_save_dir}/joblog", sep="\t")
+    return df_log
+# This works in bash
+# $ parallel -k  --link --dryrun "python fit_mojave_uvfits_single_ncomponents.py --uvfits \"uvfits\" --results_dir \"results_dir\" --ncomps {1} --maxnsaves {2}" ::: 1 2 ::: 1000 2000
+
+
 # Path to UVFITS, path to FITS, n_components
-df = pd.read_csv(table_file)
-base_UVFITS_dir = "/mnt/jet1/yyk/VLBI/RFC/images"
-# Directory to save the results
-base_save_dir = "/mnt/storage/ilya/VLBI_Gaia"
-n_jobs = 2
+table_file = "/home/ilya/data/VLBI_Gaia/full_table_ncomp_2.csv"
+n_jobs = 10
 # Average time in difmap. It uses weighted vector averaging and
 # also re-calculate weights. This is helpful when they are not reliable.
 difmap_avg_time_sec = 60
-
+# Directory to save the results
+base_save_dir = "/mnt/storage/ilya/VLBI_Gaia"
 # FIXME: Choose executable depending on ``component_type``
-executable_dict = {2: "/home/ilya/github/bam/Release/bam_2",
-                   3: "/home/ilya/github/bam/Release/bam_3"}
-executable = "/home/ilya/github/bam/Release/bam"
-
+executable_dict = {2: "/home/ilya/github/bam/Release/bam_2"}
+                   #3: "/home/ilya/github/bam/Release/bam_3"}
 template_options = "/home/ilya/github/bam/OPTIONS_gen"
 
-
-uvfits_files = list()
-ccfits_file = list()
-n_components = list()
-results_dirs = list()
-basenames = list()
-data_files = list()
-maxnsaves_list = list()
-executables = list()
-for row in df.itertuples():
-    uvfits = row.uvfits
-    ccfits = row.ccfits
-    ncomps = row.ncomps
-
-
-    uvfits_fn = os.path.split(uvfits)[-1]
-    source, band, year, month, day, author, product = uvfits_fn.split("_")
-    product = product.split(".")[0]
-    assert product == "vis"
-    save_basename = f"{source}_{band}_{year}_{month}_{day}_{author}_ncomp_{ncomps}"
-    basenames.append(save_basename)
-    results_dir = os.path.join(base_save_dir, f"{source}_{band}_{year}_{month}_{day}_{author}")
-    results_dirs.append(results_dir)
-    pathlib.Path(results_dir).mkdir(parents=True, exist_ok=True)
-    out_fname = os.path.join(results_dir, f"{source}_{band}_{year}_{month}_{day}_{author}_avg_{difmap_avg_time_sec}sec.csv")
-    with TemporaryDirectory() as working_dir:
-        df = get_data_file_from_ehtim(uvfits, out_fname, avg_time_sec=difmap_avg_time_sec, working_dir=working_dir)
-    n_vis = len(df)
-    print("Number of vis = ", n_vis)
-    print("Number of comps = ", ncomps)
-    maxnsaves = choose_maxnsaves(ncomps, n_vis)
-    print("Chosen maxnsaves = ", maxnsaves)
-    maxnsaves_list.append(str(maxnsaves))
-    n_components.append(ncomps)
-    try:
-        executables.append(executable_dict[ncomps])
-    except KeyError:
-        raise Exception(f"No executable for #components = {ncomps}!")
-    data_files.append(out_fname)
-
-
-args = " ".join(["{:s} {:s} {:s}".format(i, j, k) for (i, j, k) in zip(basenames, data_files, results_dirs)])
-
-maxnsaves = " ".join(maxnsaves_list)
-executables = " ".join(executables)
-basenames = " ".join(basenames)
-data_files = " ".join(data_files)
-results_dirs = " ".join(results_dirs)
-
-print("basenames\n", basenames)
-print("data_files\n", data_files)
-print("results_dirs\n", results_dirs)
-print("maxnsaves\n", maxnsaves)
-print("executables\n", executables)
-print("args\n", args)
-
-if dry_run:
-    os.system('parallel --files --results {}/res_{{1}} --joblog {}/joblog --jobs {} --dryrun --link '
-              '"python fit_mojave_uvfits_single_ncomponents.py --template_options {} --basename {{1}} '
-        '--data_file {{2}} --results_dir {{3}} --maxnsaves {{4}} --executable {{5}}" ::: {} ::: {} ::: {} ::: {} ::: {}'.format(base_save_dir, base_save_dir, n_jobs,
-                                                                                   template_options,
-                                                                                   basenames, data_files, results_dirs, maxnsaves, executables))
-else:
-    os.system('parallel --files --results {}/res_{{1}} --joblog {}/joblog --jobs {} --link '
-              '"python fit_mojave_uvfits_single_ncomponents.py --template_options {} --basename {{1}} '
-        '--data_file {{2}} --results_dir {{3}} --maxnsaves {{4}} --executable {{5}}" ::: {} ::: {} ::: {} ::: {} ::: {}'.format(base_save_dir, base_save_dir, n_jobs,
-                                                                                   template_options,
-                                                                                   basenames, data_files, results_dirs, maxnsaves, executables))
-
-# This works in bash
-# $ parallel -k  --link --dryrun "python fit_mojave_uvfits_single_ncomponents.py --uvfits \"uvfits\" --results_dir \"results_dir\" --ncomps {1} --maxnsaves {2}" ::: 1 2 ::: 1000 2000
+logs_list = list() 
+df_full = pd.read_csv(table_file)
+# df = df_full.head(600)
+df = df_full[:100]
+df_log = run_parallels_on_df(df, base_save_dir, executable_dict, template_options, n_jobs, difmap_avg_time_sec, dry_run=False)
+logs_list.append(df_log)
